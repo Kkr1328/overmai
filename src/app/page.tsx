bfa7566e91ccadcaae33f4b57161f5d2444a3bcc
Chinnapat.N <kharkkr@gmail.com>
2023-09-19 20:38:22 +0700

    Utilize tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,62 +4,38 @@ import Image from 'next/image';
 import { AppBar, Box, Card, Grid, Stack, Typography } from '@mui/material';
 import DirectionsCarRoundedIcon from '@mui/icons-material/DirectionsCarRounded';
 
-import CarInfo from '@/components/CarInfo';
-import { COLOR, SPACE } from '@/constant/CONST';
+import Map from '@/components/Map';
+import Carcontroller from '@/components/CarController';
 import { CARS, CarType } from '@/constant/ENTITY';
 
 export default function Home() {
 	return (
 		<Fragment>
-			<Box
-				sx={{
-					background: COLOR.DARK_GREY,
-					width: '100vw',
-					height: '100vh',
-				}}>
-				<AppBar
-					position="static"
-					sx={{ background: COLOR.BLACK, px: SPACE[16], py: SPACE[8] }}>
+			<Box className="w-screen h-screen bg-dark_grey">
+				<AppBar position="static" className="px-16 py-8 bg-black">
 					<Image src="/logo.png" alt="Logo" width={200} height={80} />
 				</AppBar>
 
-				<Stack spacing={SPACE[16]} sx={{ px: SPACE[32], py: SPACE[16] }}>
-					<Stack direction="row" spacing={SPACE[8]}>
-						<DirectionsCarRoundedIcon
-							sx={{ color: COLOR.BLACK }}
-							fontSize="large"
-						/>
+				<Stack className="px-32 py-16 gap-16">
+					<Stack direction="row" className="gap-8">
+						<DirectionsCarRoundedIcon className="text-black" fontSize="large" />
 						<Typography
 							variant="h5"
 							fontWeight="bold"
-							color={COLOR.BLACK}
+							className="text-black"
 							alignSelf="center">
 							Cars' controller
 						</Typography>
 					</Stack>
-					<Card sx={{ px: SPACE[32], py: SPACE[16] }}>
+					<Card className="px-32 py-16">
 						<Grid container spacing={2}>
-							<Grid item xs={6} justifyContent="center" alignSelf="center">
-								<Image
-									src="/path1.svg"
-									alt="path"
-									width={300}
-									height={300}
-									style={{
-										margin: 'auto',
-									}}
-								/>
-								<Stack direction="row" gap={SPACE[8]}>
-									<Image src="/car1.svg" alt="car1" width={20} height={80} />
-									<Image src="/car2.svg" alt="car2" width={20} height={80} />
-									<Image src="/car3.svg" alt="car3" width={20} height={80} />
-									<Image src="/car4.svg" alt="car4" width={20} height={80} />
-								</Stack>
+							<Grid item xs={6}>
+								<Map />
 							</Grid>
 							<Grid item xs={6}>
-								<Stack spacing={SPACE[16]} alignItems="start">
+								<Stack className="gap-16" alignItems="start">
 									{Object.entries(CARS).map(([value, name]) => (
-										<CarInfo value={value as CarType} name={name} />
+										<Carcontroller value={value as CarType} name={name} />
 									))}
 								</Stack>
 							</Grid>
