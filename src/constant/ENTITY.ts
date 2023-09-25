4b28d263e7f8d5353e3a68fecf89e19b22eac823
Chinnapat.N <kharkkr@gmail.com>
2023-09-25 15:24:55 +0700

    Update car names

--- src/constant/ENTITY.ts
+++ src/constant/ENTITY.ts
@@ -1,5 +1,15 @@
-export enum CARS { CAR1 = "Car 1", CAR2 = "Car 2", CAR3 = "Car 3", CAR4 = "Car 4"}
-export type CarType = keyof typeof CARS
+export enum CARS {
+	red_recrive = 'Car 1',
+	blue_recrive = 'Car 2',
+	green_recrive = 'Car 3',
+	black_recrive = 'Car 4',
+}
+export type CarType = keyof typeof CARS;
 
-export enum LANES { LANE1 = "Lane 1", LANE2 = "Lane 2", LANE3 = "Lane 3", LANE4 = "Lane 4"}
-export type LaneType = keyof typeof LANES
+export enum LANES {
+	LANE1 = 'Lane 1',
+	LANE2 = 'Lane 2',
+	LANE3 = 'Lane 3',
+	LANE4 = 'Lane 4',
+}
+export type LaneType = keyof typeof LANES;
